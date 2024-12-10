6cec483e4de08e56914c53be30a5c04abebb1e4c
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-12-10 11:22:55 +0500

    Update Commit

--- handlers/uzb.py
+++ handlers/uzb.py
@@ -1,13 +1,14 @@
 from aiogram import F
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, FSInputFile
 from aiogram.filters import Command
 from loader import router_user
 from keyboards.inline.main import *
 from keyboards.default.main import *
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from database.userdb import create_user, create_card
+from database.userdb import create_user, create_card, get_user, user_exists
 from random import *
+from PIL import Image, ImageDraw, ImageFont
 
 class UserAge(StatesGroup):
     user_age = State()
@@ -19,13 +20,46 @@ class UserInformations(StatesGroup):
     surname = State()
     age = State()
 
+
+def edit_card_image(carta_path, output_path, card_number, name, expiry_date):
+    # Rasmni yuklash
+    img = Image.open(carta_path)
+
+    # Rasmga chizish uchun ob'ekt
+    draw = ImageDraw.Draw(img)
+
+    font_path = "card/firecode.ttf"
+    # Shriftni yuklash
+    try:
+        font_num = ImageFont.truetype(font_path, 50)  # Windows tizimida ishlaydi
+        font_date = ImageFont.truetype(font_path, 30)  
+        font_name = ImageFont.truetype(font_path, 40)  
+    except IOError:
+        font = ImageFont.load_default()  # Agar shrift yo'q bo'lsa, tizimning default shriftini yuklaydi
+
+    # Matnlarni joylashtirish
+    draw.text((300, 450), card_number, fill="white", font=font_num)  # Karta raqami
+    draw.text((500, 530), expiry_date, fill="white", font=font_date)  # Amal qilish muddati
+    draw.text((200, 560), name, fill="white", font=font_name)         # Foydalanuvchi ismi
+
+    # O'zgartirilgan rasmni saqlash
+    img.save(output_path)
+
+
 @router_user.callback_query(F.data == "uz")
 async def start_uz(callback: CallbackQuery, state: FSMContext):
     await callback.message.answer("Assalomu aleykum!")
-    await callback.message.answer("Yoshingizni kiriting!")
-    await state.set_state(UserAge.user_age)
-    await callback.message.delete()
-    await callback.answer()  # Callbackni yopish
+    check  = await user_exists(callback.from_user.id)
+    print(check)
+    if check:
+        await callback.message.answer("Sizda karta bor!", reply_markup=menu_users)
+        await callback.message.delete()
+        await callback.answer() 
+    else:
+        await callback.message.answer("Yoshingizni kiriting!")
+        await state.set_state(UserAge.user_age)
+        await callback.message.delete()
+        await callback.answer()  # Callbackni yopish
 
 @router_user.message(UserAge.user_age)
 async def user_age(msg: Message, state: FSMContext):
@@ -79,16 +113,32 @@ async def age(msg: Message, state: FSMContext):
     surname = data['surname']
     age = data['age']
     number = data['number']
+    user_id = msg.from_user.id
 
-    # Foydalanuvchini bazaga qo'shish
-    user_id = await create_user(name, surname, age, number)
-    print(user_id)
+    # Foydalanuvchini bazaga qo'shish (loyiha talabiga ko'ra funksiya chaqirildi)
+    await create_user(user_id, name, surname, age, number)
     await msg.answer("Siz ro'yxatdan o'tdingiz!")
 
     # Karta raqami va PIN kodini yaratish
-    card_number = randint(1000000000000000, 1000000000000000)
+    card_number = randint(4470000000000000, 4471000000000000)
     card_pin = randint(1000, 9999)
-    await create_card(user_id, str(card_number), str(card_pin))  # Kartani bazaga qo'shish
+    card_number_formatted = f"{str(card_number)[:4]} {str(card_number)[4:8]} {str(card_number)[8:12]} {str(card_number)[12:]}"
+    expirydate = "01/25"
+
+    # Karta tasvirini yaratish
+    input_image_path = 'card/card.png'
+    output_image_path = 'card/output_card.png'
+    edit_card_image(input_image_path, output_image_path, card_number_formatted, name, expirydate)
+
+    # Kartani bazaga qo'shish (agar mavjud funksiya bo'lsa)
+    await create_card(user_id, card_number, card_pin)
 
     # Foydalanuvchiga karta ma'lumotlarini yuborish
-    await msg.answer(f"Your card number is {card_number} and your card PIN is {card_pin}")
+    await msg.answer_photo(
+        photo=FSInputFile(output_image_path),
+        caption=f"Your card number is <b>{card_number_formatted[:9]}******{card_number_formatted[-4:]}</b>\n"
+                f"Your card PIN is <b>{card_pin}</b>\n\n"
+                "Ogohlantiramiz bu ma'lumotlarni hech kimga oshkor qilmang!",
+        parse_mode="HTML"
+    )
+    await msg.answer("Iltimos qayta /start buyrug'ini bering!")
