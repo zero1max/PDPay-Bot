9dce570df96644852670417d6e1b8faad2c12685
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-12-21 10:50:19 +0500

    Update Commit

--- keyboards/inline/main.py
+++ keyboards/inline/main.py
@@ -66,3 +66,70 @@ ortgaqaytish = InlineKeyboardMarkup(
         [InlineKeyboardButton(text="Ortga qaytish🔙", callback_data='ortgaqaytish')]
     ]
 )
+
+# --------------------------------------------------------------------
+
+choice_language_ru = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="English🇺🇸", callback_data="en"),
+            InlineKeyboardButton(text="Русский🇷🇺", callback_data="ru"),
+            InlineKeyboardButton(text="Узбекский🇺🇿", callback_data="uz")
+        ]
+    ]
+)
+
+choice_create_card_ru = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Да", callback_data='kartaha_ru'),
+            InlineKeyboardButton(text="Нет", callback_data="kartayoq_ru")
+        ]
+    ]
+)
+
+menu_users_ru = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Сменить пароль карты🔗", callback_data="parolalmashtirish_ru")],
+        [InlineKeyboardButton(text="Проверить баланс кошелька💳", callback_data="hamyontekshirish_ru"), InlineKeyboardButton(text="Курс валюты💱", callback_data="valyutakursi_ru")],
+        [InlineKeyboardButton(text="Оплатить💰", callback_data="tolovqilish_ru"), InlineKeyboardButton(text="Перевести деньги💸", callback_data='pulotkazish_ru')],
+        [InlineKeyboardButton(text="Получить информацию о карте📲", callback_data="kartamalumotlariolish_ru")],
+        [InlineKeyboardButton(text="Выйти🔙", callback_data='chiqish_ru')]
+    ]
+)
+
+menu_tolov_qilish_ru = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Оплатить мобильному оператору 📱", callback_data='mobile_ru')],
+        [InlineKeyboardButton(text="Оплатить коммунальные услуги 💡", callback_data='komunal_ru')],
+        [InlineKeyboardButton(text="Назад🔙", callback_data='ortgaqaytish_ru')]
+    ]
+)
+
+menu_komunal_ru = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="⚡ Электричество", callback_data='elektr_ru'), InlineKeyboardButton(text='🔥 Газ', callback_data='gaz_ru')],
+        [InlineKeyboardButton(text="🏫 Школа", callback_data='school_ru'), InlineKeyboardButton(text='💧 Горячая вода', callback_data="issiqsuv_ru")],
+        [InlineKeyboardButton(text='🌐 Интернет провайдер', callback_data='internet_ru'), InlineKeyboardButton(text='🗑️ Мусор', callback_data="axlat_ru")],
+        [InlineKeyboardButton(text="Назад🔙", callback_data='ortgaqaytish_ru')]
+    ]
+)
+
+payorget_ru = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Пополнить кошелек➕", callback_data='hamyontoldirish_ru')],
+        [InlineKeyboardButton(text="Снять деньги➖", callback_data='pulyechibolish_ru')]
+    ]
+)
+
+izohyes_no_ru = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Да', callback_data='izohyes_ru'), InlineKeyboardButton(text="Нет", callback_data='izohno_ru')]
+    ]
+)
+
+ortgaqaytish_ru = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Назад🔙", callback_data='ortgaqaytish_ru')]
+    ]
+)
